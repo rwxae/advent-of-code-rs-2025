394458a3e293325af05025111954d2f30d707153
wardxela <wardxela@gmail.com>
2025-12-31 23:27:01 +0300

    feat: 1st day (2/2)

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,33 +1,70 @@
 use advent_of_code_2025::read_input;
 
+const START_STATE: i32 = 50;
+const START_COUNT: u32 = 0;
+
 fn main() {
-    let answer_1_easy = solution_1(&read_input("01-easy.txt"));
-    let answer_1_hard = solution_1(&read_input("01-hard.txt"));
+    let input_easy = read_input("01-easy.txt");
+    let input_hard = read_input("01-hard.txt");
+
+    let answer_1_easy = solution_1(&input_easy);
+    let answer_1_hard = solution_1(&input_hard);
+    let answer_2_easy = solution_2(&input_easy);
+    let answer_2_hard = solution_2(&input_hard);
 
     println!("Answer 1 (easy): {answer_1_easy}");
     println!("Answer 1 (hard): {answer_1_hard}");
+    println!("Answer 2 (easy): {answer_2_easy}");
+    println!("Answer 2 (hard): {answer_2_hard}");
 }
 
 fn solution_1(input: &str) -> u32 {
     input
         .trim()
         .lines()
-        .map(|line| (&line[0..1], line[1..].parse::<u32>().unwrap()))
-        .fold((50i32, 0), |acc, rotation| {
+        .map(|line| {
+            (
+                line.chars().next().unwrap(),
+                line[1..].parse::<u32>().unwrap(),
+            )
+        })
+        .fold((START_COUNT, START_STATE), |acc, rotation| {
             let state = match rotation.0 {
-                "L" => ((acc.0 - 100 - (rotation.1 as i32)) % 100 + 100) % 100,
-                "R" => (acc.0 + (rotation.1 as i32)) % 100,
+                'L' => ((acc.1 - 100 - (rotation.1 as i32)) % 100 + 100) % 100,
+                'R' => (acc.1 + (rotation.1 as i32)) % 100,
                 _ => panic!("Unsupported rotation"),
             };
-            let count = if state == 0 { acc.1 + 1 } else { acc.1 };
-            (state, count)
+            let count = if state == 0 { acc.0 + 1 } else { acc.0 };
+            (count, state)
         })
-        .1
+        .0
 }
 
-// fn solution_2(input: &str) -> u32 {
-//     42
-// }
+fn solution_2(input: &str) -> u32 {
+    input
+        .trim()
+        .lines()
+        .map(|line| {
+            (
+                line.chars().next().unwrap(),
+                line[1..].parse::<u32>().unwrap(),
+            )
+        })
+        .fold((START_COUNT, START_STATE), |acc, rotation| {
+            match rotation.0 {
+                'L' => (
+                    (acc.0 as i32 - ((((acc.1 - 100) % 100) - (rotation.1 as i32)) / 100)) as u32,
+                    (((acc.1 - 100 - (rotation.1 as i32)) % 100) + 100) % 100,
+                ),
+                'R' => (
+                    acc.0 + ((acc.1 + rotation.1 as i32) / 100) as u32,
+                    (acc.1 + (rotation.1 as i32)) % 100,
+                ),
+                _ => panic!("Unsupported rotation"),
+            }
+        })
+        .0
+}
 
 #[cfg(test)]
 mod tests {
@@ -41,5 +78,15 @@ mod tests {
     #[test]
     fn test_1_hard() {
         assert_eq!(solution_1(&read_input("01-hard.txt")), 1180);
+    }
+
+    #[test]
+    fn test_2_easy() {
+        assert_eq!(solution_2(&read_input("01-easy.txt")), 6);
+    }
+
+    #[test]
+    fn test_2_hard() {
+        assert_eq!(solution_2(&read_input("01-hard.txt")), 6892);
     }
 }
