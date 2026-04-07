b9f5ed35734256c469077eb10e5d85934d6174c0
wardxela <wardxela@gmail.com>
2026-04-07 19:44:32 +0300

    feat: day 4 (2/2)

--- src/day_04.rs
+++ src/day_04.rs
@@ -1,7 +1,3 @@
-fn grid_get(m: &Vec<&str>, i: usize, j: usize) -> Option<char> {
-    m.get(i)?.chars().nth(j)
-}
-
 const COMBINATIONS: [(i64, i64); 8] = [
     (-1, -1),
     (-1, 0),
@@ -25,7 +21,10 @@ pub fn solution_1(input: &str) -> u32 {
             for (i1, j1) in COMBINATIONS {
                 let i = ((i as i64) + i1) as usize;
                 let j = ((j as i64) + j1) as usize;
-                let is_paper = grid_get(&grid, i, j).map(|ch| ch == '@').unwrap_or(false);
+                let is_paper = grid
+                    .get(i)
+                    .and_then(|line| line.chars().nth(j).map(|v| v == '@'))
+                    .unwrap_or(false);
                 if is_paper {
                     counter += 1;
                 }
@@ -39,7 +38,45 @@ pub fn solution_1(input: &str) -> u32 {
 }
 
 pub fn solution_2(input: &str) -> u64 {
-    todo!()
+    let mut grid: Vec<Vec<bool>> = input
+        .split_whitespace()
+        .map(|line| {
+            line.chars()
+                .map(|ch| match ch {
+                    '@' => true,
+                    _ => false,
+                })
+                .collect()
+        })
+        .collect();
+    let mut result = 0;
+    let mut i = 0;
+    'main: while i < grid.len() {
+        for j in 0..grid[i].len() {
+            if !grid[i][j] {
+                continue;
+            }
+            let mut counter = 0;
+            for (i1, j1) in COMBINATIONS {
+                let i = ((i as i64) + i1) as usize;
+                let j = ((j as i64) + j1) as usize;
+                let is_paper = *grid.get(i).and_then(|line| line.get(j)).unwrap_or(&false);
+                if is_paper {
+                    counter += 1;
+                }
+            }
+            if counter < 4 {
+                result += 1;
+                grid[i][j] = false;
+                if i > 0 {
+                    i -= 1;
+                    continue 'main;
+                }
+            }
+        }
+        i += 1;
+    }
+    result
 }
 
 #[cfg(test)]
@@ -59,11 +96,11 @@ mod tests {
 
     #[test]
     fn test_2_easy() {
-        assert_eq!(solve(4, 1, solution_2), 42);
+        assert_eq!(solve(4, 1, solution_2), 43);
     }
 
     #[test]
     fn test_2_hard() {
-        assert_eq!(solve(4, 2, solution_2), 42);
+        assert_eq!(solve(4, 2, solution_2), 8557);
     }
 }
